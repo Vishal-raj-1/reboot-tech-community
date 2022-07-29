f5323ce7b294be36391981da949ba005abfaf9ab
Vishal-raj-1 <rajputvishal33786@gmail.com>
2022-07-30 01:17:08 +0530

    fixed founder image

--- components/aboutFounder.tsx
+++ components/aboutFounder.tsx
@@ -14,7 +14,6 @@ const AboutFounder = () => {
                         className='founderImage'
                     />
                 </Grid>
-<<<<<<< HEAD
                 <Grid item xs={12} md={6}>
                     <Card className='glass'>
                         <CardContent>
@@ -30,21 +29,6 @@ const AboutFounder = () => {
                             <Socials />
                         </CardContent>
                     </Card>
-=======
-                <Grid item xs={12} sm={8} md={5}>
-                    <CardContent>
-                        <Typography mb={5} variant="h3" align='center'>
-                            About Founder
-                        </Typography>
-                        <Typography mb={2} align='center'>
-                            Vishal Rajput is frontend developer from India. He has worked for 5 startups in last 2 years.
-                        </Typography>
-                        <Typography mb={3} align='center'>
-                            He has mentored 1000+ students regarding web development, data structure and algorithms, open source, interview preperation and lot more.
-                        </Typography>
-                        <Socials />
-                    </CardContent>
->>>>>>> 200164fd31486a84c53c966b8e9f8eda003177b1
                 </Grid>
             </Grid>
         </Container>
