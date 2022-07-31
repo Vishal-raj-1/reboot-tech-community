8d6ef142eaa193f2c8b041f110f14abe1ec2ae38
Vishal-raj-1 <rajputvishal33786@gmail.com>
2022-07-31 14:43:21 +0530

    adding link of source code of courses

--- components/courses.tsx
+++ components/courses.tsx
@@ -3,8 +3,8 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Container, Chip, CardActions, Grid, Button } from '@mui/material';
-import { YouTube, Visibility } from '@mui/icons-material';
+import { Container, CardActions, Grid, Button } from '@mui/material';
+import { YouTube, NoteAlt } from '@mui/icons-material';
 import styles from '../styles/home.module.css';
 
 function Courses() {
@@ -15,13 +15,15 @@ function Courses() {
             title: 'Frontend Development Essential Course',
             description: 'In Frontend Development Essential Course, we will learn HTML, CSS, CSS FlexBox, CSS Grid, JavaScript, Asynchronous Programming, Git & GitHub. We will also build 5+ projects to understand each and every topic. Finally, We will build our portfolio and also learn how to host our website on internet for FREE.',
             youtubeLink: 'https://www.youtube.com/watch?v=YCbCGuq_NWE&list=PLSH9gf0XETou_b8C3SJm8rk69Z41eUgbQ&index=1',
+            notesLink: 'https://github.com/Vishal-raj-1/Frontend-Development-Essentials'
         },
         {
             id: 1,
             image: 'binaryTree',
             title: 'Binary Trees',
             description: 'In this course, We will deep dive into Binary Tree Data Structure. We will start from Binary Tree Traversals, solve some basic fundamental questions then gradually increase the dificulty level of question and solve most frequently asked interview questions too. We will solve questions using multiple approaches i.e, Bottom Up, Top Down, Recursive & Iterative.',
-            youtubeLink: 'https://www.youtube.com/watch?v=PBcgUy-XI-A&list=PLSH9gf0XETotuua1g7YYXWfZXZnU_LB5S&index=1'
+            youtubeLink: 'https://www.youtube.com/watch?v=PBcgUy-XI-A&list=PLSH9gf0XETotuua1g7YYXWfZXZnU_LB5S&index=1',
+            notesLink: 'https://github.com/Vishal-raj-1/DSA-for-Placement-Internship/tree/main/Binary%20Tree'
         },
         {
             id: 2,
@@ -29,6 +31,7 @@ function Courses() {
             title: 'Master Linked List',
             description: 'In Master Linked List course, we will start from learning fundamentals operation on linked list, then we will solve classical easy questions and then we will move to medium hard level problems. We will learn in depth, how to use of two pointer approach and dummy node.',
             youtubeLink: 'https://www.youtube.com/watch?v=c8a4sRSPJvU&list=PLSH9gf0XETourRyZW56Rdh9e0Phx-AJM5&index=1',
+            notesLink: 'https://github.com/Vishal-raj-1/DSA-for-Placement-Internship/tree/main/Linked%20List'
         },
         {
             id: 3,
@@ -36,6 +39,7 @@ function Courses() {
             title: 'Stack Data Structure',
             description: 'In this playlist, we will be learn about some pattern of stack questions. We are going to solve easy, medium and hard level problems. We use stack for Depth first Search, so we will solve more of stack question in Binary Tree course.',
             youtubeLink: 'https://www.youtube.com/watch?v=saN4Mi_rXfs&list=PLSH9gf0XETotSpywVcJGIYODBNL_j0P0u&index=1',
+            notesLink: 'https://github.com/Vishal-raj-1/DSA-for-Placement-Internship/tree/main/Stack'
         },
         {
             id: 4,
@@ -43,6 +47,7 @@ function Courses() {
             title: 'HashMap & HashSet',
             description: 'In this course, we will be learn fundamental design of hashset and hashmap, then we will solve classical array and string question on hashmap. Priority Queue with hashmap is one of the important topic for interview preperation. That we are going to learn in this course.',
             youtubeLink: 'https://www.youtube.com/watch?v=QfgH3_MmrOM&list=PLSH9gf0XETouStSgwNw_7YCHhRgQU4lvd&index=1',
+            notesLink: 'https://github.com/Vishal-raj-1/DSA-for-Placement-Internship/tree/main/HashMap%20%26%20HashSet'
         },
     ]
     return (
@@ -74,13 +79,15 @@ function Courses() {
                             <CardActions sx={{m: '5px auto'}}>
                                     <a href={course.youtubeLink} target='_blank'>
                                         <Button variant='contained' color='error'>
-                                            <YouTube sx={{mr: '10px'}}/> Checkout Playlist
+                                            <YouTube sx={{mr: '5px'}}/> Live Course
                                         </Button>
                                     </a>
                                     
-                                    {/* <a href={course.youtubeLink} target='_blank'>
-                                        <Chip icon={<Visibility />} label="View More" variant='filled' sx={{ my: 'auto', cursor: 'pointer'}} />
-                                    </a> */}
+                                    <a href={course.notesLink} target='_blank'>
+                                        <Button variant='contained' color='primary'>
+                                            <NoteAlt sx={{mr: '5px'}}/> Source Code
+                                        </Button>
+                                    </a>
                             </CardActions>
                         </Card>
                     </Grid>
